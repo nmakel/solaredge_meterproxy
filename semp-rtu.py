f8f7b763b3517e7c55af327c264d814b1f4adf28
niels <51261230+nmakel@users.noreply.github.com>
2020-07-18 20:24:37 +0200

    moved init of static register chunks outside of t_update

--- semp-rtu.py
+++ semp-rtu.py
@@ -85,72 +85,17 @@ def t_update(ctx, config, stop, module, device):
             block_1101.add_32bit_float(values.get("maximum_demand_power_active", 0)) # maximum demand power
             block_1101.add_32bit_float(values.get("demand_power_apparent", 0)) # apparent demand power
             block_1101.add_32bit_float(values.get("p1_demand_power_active", 0)) # demand power l1
-            block_1101.add_32bit_float(values.get("p2_demand_power_active", 0)) # demand power l2 
+            block_1101.add_32bit_float(values.get("p2_demand_power_active", 0)) # demand power l2
             block_1101.add_32bit_float(values.get("p3_demand_power_active", 0)) # demand power l3
             ctx.setValues(3, 1100, block_1101.to_registers())
-
-            block_1601 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Little)
-            block_1601.add_32bit_int(1234) # config passcode
-            block_1601.add_16bit_int(config["ct_current"]) # ct rated current
-            block_1601.add_16bit_int(config["p1_ct_current"]) # ct rated current l1
-            block_1601.add_16bit_int(config["p2_ct_current"]) # ct rated current l2
-            block_1601.add_16bit_int(config["p3_ct_current"]) # ct rated current l3
-            block_1601.add_16bit_int(config["ct_inverted"]) # ct direction inversion
-            block_1601.add_16bit_int(0) # measurement averaging
-            block_1601.add_16bit_int(0) # power scale
-            block_1601.add_16bit_int(15) # demand period
-            block_1601.add_16bit_int(1) # demand subintervals
-            block_1601.add_16bit_int(10000) # power/energy adjustment l1
-            block_1601.add_16bit_int(10000) # power/energy adjustment l2
-            block_1601.add_16bit_int(10000) # power/energy adjustment l3
-            block_1601.add_16bit_int(-1000) # ct phase angle adjustment l1
-            block_1601.add_16bit_int(-1000) # ct phase angle adjustment l2
-            block_1601.add_16bit_int(-1000) # ct phase angle adjustment l3
-            block_1601.add_16bit_int(1500) # minimum power reading
-            block_1601.add_16bit_int(config["phase_offset"]) # phase offset
-            block_1601.add_16bit_int(0) # reset energy
-            block_1601.add_16bit_int(0) # reset demand
-            block_1601.add_16bit_int(20000) # current scale
-            block_1601.add_16bit_int(0) # io pin mode
-            ctx.setValues(3, 1600, block_1601.to_registers())
-
-            block_1651 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Little)
-            block_1651.add_16bit_int(0) # apply config
-            block_1651.add_16bit_int(config["address"]) # modbus address
-            block_1651.add_16bit_int(4) # baud rate
-            block_1651.add_16bit_int(0) # parity mode
-            block_1651.add_16bit_int(0) # modbus mode
-            block_1651.add_16bit_int(5) # message delay
-            ctx.setValues(3, 1650, block_1651.to_registers())
-
-            block_1701 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Little)
-            block_1701.add_32bit_int(config["serial_number"]) # serial number
-            block_1701.add_32bit_int(0) # uptime (s)
-            block_1701.add_32bit_int(0) # total uptime (s)
-            block_1701.add_16bit_int(0) # wattnode model
-            block_1701.add_16bit_int(25) # firmware version
-            block_1701.add_16bit_int(0) # wattnode options
-            block_1701.add_16bit_int(0) # error status
-            block_1701.add_16bit_int(0) # power fail count
-            block_1701.add_16bit_int(0) # crc error count
-            block_1701.add_16bit_int(0) # frame error count
-            block_1701.add_16bit_int(0) # packet error count
-            block_1701.add_16bit_int(0) # overrun count
-            block_1701.add_16bit_int(0) # error status 1
-            block_1701.add_16bit_int(0) # error status 2
-            block_1701.add_16bit_int(0) # error status 3
-            block_1701.add_16bit_int(0) # error status 4
-            block_1701.add_16bit_int(0) # error status 5
-            block_1701.add_16bit_int(0) # error status 6
-            block_1701.add_16bit_int(0) # error status 7
-            block_1701.add_16bit_int(0) # error status 8
-            ctx.setValues(3, 1700, block_1701.to_registers())
+        except Exception as e:
+            logger.critical(f"{this_t.name}: {e}")
         finally:
             time.sleep(1)
 
 
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser()   
+    argparser = argparse.ArgumentParser()
     argparser.add_argument("-c", "--config", type=str, default="semp.conf")
     argparser.add_argument("-v", "--verbose", action="store_true", default=False)
     args = argparser.parse_args()
@@ -159,12 +104,12 @@ if __name__ == "__main__":
         "server": {
             "device": "/dev/ttyUSB0",
             "log_file": "semp.log",
-            "log_level": "DEBUG",
+            "log_level": "INFO",
             "meters": ''
         },
         "meters": {
             "src_address": 1,
-            "dst_address": 2, 
+            "dst_address": 2,
             "type": "generic",
             "stopbits": 1,
             "parity": "e",
@@ -221,47 +166,92 @@ if __name__ == "__main__":
                 unit=confparser[meter].getint("src_address", fallback=default_config["meters"]["src_address"])
             )
 
-            logger.debug(f"Meter {address} created: {meter_device}")
-
             slave_ctx = ModbusSlaveContext()
-
             update_t_stop = threading.Event()
             update_t = threading.Thread(
                 target=t_update,
                 name=f"t_update_{address}",
                 args=(
                     slave_ctx,
-                    {
-                        "address": address,
-                        "ct_current": confparser[meter].getint("ct_current", fallback=default_config["meters"]["ct_current"]),
-                        "p1_ct_current": confparser[meter].getint("p1_ct_current", fallback=default_config["meters"]["p1_ct_current"]),
-                        "p2_ct_current": confparser[meter].getint("p2_ct_current", fallback=default_config["meters"]["p2_ct_current"]),
-                        "p3_ct_current": confparser[meter].getint("p3_ct_current", fallback=default_config["meters"]["p3_ct_current"]),
-                        "ct_inverted": confparser[meter].getint("ct_inverted", fallback=default_config["meters"]["ct_inverted"]),
-                        "phase_offset": confparser[meter].getint("phase_offset", fallback=default_config["meters"]["phase_offset"]),
-                        "serial_number": confparser[meter].getint("serial_number", fallback=default_config["meters"]["serial_number"]),
-                    },
                     update_t_stop,
                     meter_module,
                     meter_device
                 )
             )
-            
+
+            block_1601 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Little)
+            block_1601.add_32bit_int(1234) # config passcode
+            block_1601.add_16bit_int(confparser[meter].getint("ct_current", fallback=default_config["meters"]["ct_current"])) # ct rated current
+            block_1601.add_16bit_int(confparser[meter].getint("p1_ct_current", fallback=default_config["meters"]["p1_ct_current"])) # ct rated current l1
+            block_1601.add_16bit_int(confparser[meter].getint("p2_ct_current", fallback=default_config["meters"]["p2_ct_current"])) # ct rated current l2
+            block_1601.add_16bit_int(confparser[meter].getint("p3_ct_current", fallback=default_config["meters"]["p3_ct_current"])) # ct rated current l3
+            block_1601.add_16bit_int(confparser[meter].getint("ct_inverted", fallback=default_config["meters"]["ct_inverted"])) # ct direction inversion
+            block_1601.add_16bit_int(0) # measurement averaging
+            block_1601.add_16bit_int(0) # power scale
+            block_1601.add_16bit_int(15) # demand period
+            block_1601.add_16bit_int(1) # demand subintervals
+            block_1601.add_16bit_int(10000) # power/energy adjustment l1
+            block_1601.add_16bit_int(10000) # power/energy adjustment l2
+            block_1601.add_16bit_int(10000) # power/energy adjustment l3
+            block_1601.add_16bit_int(-1000) # ct phase angle adjustment l1
+            block_1601.add_16bit_int(-1000) # ct phase angle adjustment l2
+            block_1601.add_16bit_int(-1000) # ct phase angle adjustment l3
+            block_1601.add_16bit_int(1500) # minimum power reading
+            block_1601.add_16bit_int(confparser[meter].getint("phase_offset", fallback=default_config["meters"]["phase_offset"])) # phase offset
+            block_1601.add_16bit_int(0) # reset energy
+            block_1601.add_16bit_int(0) # reset demand
+            block_1601.add_16bit_int(20000) # current scale
+            block_1601.add_16bit_int(0) # io pin mode
+            slave_ctx.setValues(3, 1600, block_1601.to_registers())
+
+            block_1651 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Little)
+            block_1651.add_16bit_int(0) # apply config
+            block_1651.add_16bit_int(address) # modbus address
+            block_1651.add_16bit_int(4) # baud rate
+            block_1651.add_16bit_int(0) # parity mode
+            block_1651.add_16bit_int(0) # modbus mode
+            block_1651.add_16bit_int(5) # message delay
+            slave_ctx.setValues(3, 1650, block_1651.to_registers())
+
+            block_1701 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Little)
+            block_1701.add_32bit_int(confparser[meter].getint("serial_number", fallback=default_config["meters"]["serial_number"])) # serial number
+            block_1701.add_32bit_int(0) # uptime (s)
+            block_1701.add_32bit_int(0) # total uptime (s)
+            block_1701.add_16bit_int(0) # wattnode model
+            block_1701.add_16bit_int(25) # firmware version
+            block_1701.add_16bit_int(0) # wattnode options
+            block_1701.add_16bit_int(0) # error status
+            block_1701.add_16bit_int(0) # power fail count
+            block_1701.add_16bit_int(0) # crc error count
+            block_1701.add_16bit_int(0) # frame error count
+            block_1701.add_16bit_int(0) # packet error count
+            block_1701.add_16bit_int(0) # overrun count
+            block_1701.add_16bit_int(0) # error status 1
+            block_1701.add_16bit_int(0) # error status 2
+            block_1701.add_16bit_int(0) # error status 3
+            block_1701.add_16bit_int(0) # error status 4
+            block_1701.add_16bit_int(0) # error status 5
+            block_1701.add_16bit_int(0) # error status 6
+            block_1701.add_16bit_int(0) # error status 7
+            block_1701.add_16bit_int(0) # error status 8
+            slave_ctx.setValues(3, 1700, block_1701.to_registers())
+
             threads.append(update_t)
             thread_stops.append(update_t_stop)
 
             slaves.update({address: slave_ctx})
-            logger.debug(f"Thread created {update_t}: {meter} {address}: {meter_type} {meter_device}")
+            logger.info(f"Created {update_t}: {meter} {meter_type} {meter_device}")
 
         device = confparser["server"].get("device", fallback=default_config["server"]["device"])
         identity = ModbusDeviceIdentification()
         server_ctx = ModbusServerContext(slaves=slaves, single=False)
 
+        time.sleep(1)
+
         for t in threads:
             t.start()
-            logger.debug(f"Started {t}")
+            logger.info(f"Starting {t}")
 
-        logger.debug(f"Starting ModbusRTU serial server on {device}")
         StartSerialServer(
             server_ctx,
             framer=ModbusRtuFramer,
