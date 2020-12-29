32f9a04ec4600958a07bee2c4428b6b9017e938b
niels <51261230+nmakel@users.noreply.github.com>
2020-12-29 10:24:54 +0100

    add logging and flexible device selection to influxdb device

--- devices/influxdb.py
+++ devices/influxdb.py
@@ -46,64 +46,74 @@ def values(device):
     if not device:
         return {}
 
-    values = list(device["client"].query(
-        f'SELECT last(*) FROM "{device["bucket"]}" WHERE ("{device["where_key"]}" = \'{device["where_value"]}\')'
-    ).get_points())[0]
+    logger = logging.getLogger()
+    logger.debug(f"device: {device}")
+    
+    if device["where_key"] and device["where_value"]:
+        values = list(device["client"].query(
+            f'SELECT last(*) FROM "{device["bucket"]}" WHERE ("{device["where_key"]}" = \'{device["where_value"]}\')'
+        ).get_points())[0]
+    else:
+        values = list(device["client"].query(
+            f'SELECT last(*) FROM "{device["bucket"]}"'
+        ).get_points())[0]
+
+    logger.debug(f"values: {values}")
 
     return {
         "energy_active": values.get("last_total_energy_active", 0),
         "import_energy_active": values.get("last_import_energy_active", 0),
         "power_active": values.get("last_power_active", 0),
-        "p1_power_active": values.get("last_p1_power_active", 0),
-        "p2_power_active": values.get("last_p2_power_active", 0),
-        "p3_power_active": values.get("last_p3_power_active", 0),
+        "p1_power_active": values.get("last_power_active", 0),
+        # "p2_power_active"
+        # "p3_power_active"
         "voltage_ln": values.get("last_voltage", 0),
-        "p1n_voltage": values.get("last_p1n_voltage", 0),
-        "p2n_voltage": values.get("last_p2n_voltage", 0),
-        "p3n_voltage": values.get("last_p3n_voltage", 0),
-        "voltage_ll": values.get("last_voltage_ll", 0),
-        "p12_voltage": values.get("last_p12_voltage", 0),
-        "p23_voltage": values.get("last_p23_voltage", 0),
-        "p31_voltage": values.get("last_p31_voltage", 0),
+        "p1n_voltage": values.get("last_voltage", 0),
+        # "p2n_voltage"
+        # "p3n_voltage"
+        # "voltage_ll"
+        # "p12_voltage"
+        # "p23_voltage"
+        # "p31_voltage"
         "frequency": values.get("last_frequency", 0),
-        "p1_energy_active": values.get("last_p1_energy_active", 0),
-        "p2_energy_active": values.get("last_p2_energy_active", 0),
-        "p3_energy_active": values.get("last_p3_energy_active", 0),
-        "p1_import_energy_active": values.get("last_p1_import_energy_active", 0),
-        "p2_import_energy_active": values.get("last_p2_import_energy_active", 0),
-        "p3_import_energy_active": values.get("last_p3_import_energy_active", 0),
+        "p1_energy_active": values.get("last_total_energy_active", 0),
+        # "p2_energy_active"
+        # "p3_energy_active"
+        "p1_import_energy_active": values.get("last_import_energy_active", 0),
+        # "p2_import_energy_active"
+        # "p3_import_energy_active"
         "export_energy_active": values.get("last_export_energy_active", 0),
-        "p1_export_energy_active": values.get("last_p1_export_energy_active", 0),
-        "p2_export_energy_active": values.get("last_p2_export_energy_active", 0),
-        "p3_export_energy_active": values.get("last_p3_export_energy_active", 0),
+        "p1_export_energy_active": values.get("last_export_energy_active", 0),
+        # "p2_export_energy_active"
+        # "p3_export_energy_active"
         "energy_reactive": values.get("last_total_energy_reactive", 0),
-        "p1_energy_reactive": values.get("last_p1_energy_reactive", 0),
-        "p2_energy_reactive": values.get("last_p2_energy_reactive", 0),
-        "p3_energy_reactive": values.get("last_p3_energy_reactive", 0),
-        "energy_apparent": values.get("last_energy_apparent", 0),
-        "p1_energy_apparent": values.get("last_p1_energy_apparent", 0),
-        "p2_energy_apparent": values.get("last_p2_energy_apparent", 0),
-        "p3_energy_apparent": values.get("last_p3_energy_apparent", 0),
+        "p1_energy_reactive": values.get("last_total_energy_reactive", 0),
+        # "p2_energy_reactive"
+        # "p3_energy_reactive"
+        # "energy_apparent"
+        # "p1_energy_apparent"
+        # "p2_energy_apparent"
+        # "p3_energy_apparent"
         "power_factor": values.get("last_power_factor", 0),
-        "p1_power_factor": values.get("last_p1_power_factor", 0),
-        "p2_power_factor": values.get("last_p2_power_factor", 0),
-        "p3_power_factor": values.get("last_p3_power_factor", 0),
+        "p1_power_factor": values.get("last_power_factor", 0),
+        # "p2_power_factor"
+        # "p3_power_factor"
         "power_reactive": values.get("last_power_reactive", 0),
-        "p1_power_reactive": values.get("last_p1_power_reactive", 0),
-        "p2_power_reactive": values.get("last_p2_power_reactive", 0),
-        "p3_power_reactive": values.get("last_p3_power_reactive", 0),
+        "p1_power_reactive": values.get("last_power_reactive", 0),
+        # "p2_power_reactive"
+        # "p3_power_reactive"
         "power_apparent": values.get("last_power_apparent", 0),
-        "p1_power_apparent": values.get("last_p1_power_apparent", 0),
-        "p2_power_apparent": values.get("last_p2_power_apparent", 0),
-        "p3_power_apparent": values.get("last_p3_power_apparent", 0),
-        "p1_current": values.get("last_p1_current", 0),
-        "p2_current": values.get("last_p2_current", 0),
-        "p3_current": values.get("last_p3_current", 0),
+        "p1_power_apparent": values.get("last_power_apparent", 0),
+        # "p2_power_apparent"
+        # "p3_power_apparent"
+        "p1_current": values.get("last_current", 0),
+        # "p2_current"
+        # "p3_current"
         "demand_power_active": values.get("last_total_demand_power_active", 0),
-        "minimum_demand_power_active": values.get("last_minimum_demand_power_active", 0),
+        # "minimum_demand_power_active"
         "maximum_demand_power_active": values.get("last_maximum_total_demand_power_active", 0),
-        "demand_power_apparent": values.get("last_demand_power_apparent", 0),
+        # "demand_power_apparent"
         "p1_demand_power_active": values.get("last_total_demand_power_active", 0),
-        "p2_demand_power_active": values.get("last_p2_demand_power_active", 0),
-        "p3_demand_power_active": values.get("last_p3_demand_power_active", 0),
+        # "p2_demand_power_active"
+        # "p3_demand_power_active"
     }
